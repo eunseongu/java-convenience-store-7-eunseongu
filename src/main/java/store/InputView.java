4cc2320bfcc1e09945265000206bbe2cfc042ed4
eunseongu <rndmstjs75@gmail.com>
2024-11-08 15:17:56 +0900

    feat: item 객체를 구현한다.

--- src/main/java/store/InputView.java
+++ src/main/java/store/InputView.java
@@ -16,15 +16,15 @@ public class InputView {
         return input;
     }
 
-    public String confirmAdditionalProduct(String productName){
-        System.out.printf("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n",productName);
+    public String confirmAdditionalProduct(String itemName){
+        System.out.printf("현재 %s은(는) 1개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)%n", itemName);
         String input = Console.readLine();
 
         return input;
     }
 
-    public String confirmNoPromotion(String productName,int productCount){
-        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n",productName, productCount);
+    public String confirmNoPromotion(String itemName, int itemCount){
+        System.out.printf("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)%n", itemName, itemCount);
         String input = Console.readLine();
 
         return input;
---
+++ src/main/java/store/Item.java
@@ -0,0 +1,11 @@
+package store;
+
+public class Item {
+    private String name;
+    private int count;
+
+    public Item(String name, int quantity) {
+        this.name = name;
+        this.count = quantity;
+    }
+}
